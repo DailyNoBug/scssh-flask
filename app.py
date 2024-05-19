78bc7795c926ead1df8c10d1322d4f7b662c5b66
lsc <2790099263@qq.com>
2024-05-20 03:14:32 +0800

    update

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request, jsonify
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, emit, join_room, leave_room
 import paramiko
+import threading
 import base64
+import time
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
@@ -24,7 +26,7 @@ def add_ssh():
 
     ssh_id = f"{username}@{host}:{port}"
 
-    ssh_clients[ssh_id] = {'client': None, 'thread': None}
+    ssh_clients[ssh_id] = {'client': None, 'chan': None, 'thread': None}
 
     def ssh_thread(ssh_id, host, port, username, password, pub_key):
         client = paramiko.SSHClient()
@@ -40,18 +42,21 @@ def add_ssh():
             emit('ssh_error', {'message': str(e)}, namespace='/', to=ssh_id)
             return
 
-        ssh_clients[ssh_id]['client'] = client
         chan = client.invoke_shell()
         chan.setblocking(0)
+
+        ssh_clients[ssh_id]['client'] = client
+        ssh_clients[ssh_id]['chan'] = chan
 
         while True:
             if chan.recv_ready():
                 data = chan.recv(1024).decode()
-                socketio.emit('ssh_output', {'output': data}, namespace='/', to=ssh_id)
+                socketio.emit('ssh_output', {'output': data, 'ssh_id': ssh_id}, room=ssh_id)
 
-            socketio.sleep(0.1)
+            time.sleep(0.1)
 
-    ssh_clients[ssh_id]['thread'] = socketio.start_background_task(ssh_thread, ssh_id, host, port, username, password, pub_key)
+    ssh_clients[ssh_id]['thread'] = threading.Thread(target=ssh_thread, args=(ssh_id, host, port, username, password, pub_key))
+    ssh_clients[ssh_id]['thread'].start()
 
     return jsonify({'ssh_id': ssh_id})
 
@@ -60,9 +65,19 @@ def handle_ssh_input(json):
     ssh_id = json['ssh_id']
     input_data = json['input']
 
-    if ssh_id in ssh_clients and ssh_clients[ssh_id]['client']:
-        chan = ssh_clients[ssh_id]['client'].invoke_shell()
+    if ssh_id in ssh_clients and ssh_clients[ssh_id]['chan']:
+        chan = ssh_clients[ssh_id]['chan']
         chan.send(input_data)
+
+@socketio.on('join')
+def on_join(data):
+    ssh_id = data['ssh_id']
+    join_room(ssh_id)
+
+@socketio.on('leave')
+def on_leave(data):
+    ssh_id = data['ssh_id']
+    leave_room(ssh_id)
 
 @app.route('/delete_ssh', methods=['POST'])
 def delete_ssh():
